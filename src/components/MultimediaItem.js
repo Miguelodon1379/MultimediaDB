cfd459e45f73a73e147111b53e2c0fd08da92717
kaquka <avalos_luisangel@hotmail.com>
2024-04-30 01:38:22 -0600

    Finish2

--- src/components/MultimediaItem.js
+++ src/components/MultimediaItem.js
@@ -4,8 +4,8 @@ function MultimediaItem({ item }) {
 
   return (
     <div className={`multimedia-item ${item.filetype.charAt(0)}`}>
-      <div className={`${item.filetype}-container`}>
-        <img src={item.cover_url} alt="Thumbnail" className={`${item.filetype}-thumbnail`} />
+      <div className={`${item.filetype}-container`} styles={{top: 100}}>
+        <img src={item.cover_url} alt="Thumbnail ml-1" style={{width: '40%', height: '40%'}} />
         <div className={`${item.filetype}-details`}>
           <h2>{item.titulo}</h2>
           <p>Autor: {item.autor}</p>
@@ -24,7 +24,7 @@ function renderizarContenido(item) {
     case 'audio':
       return <audio controls src={item.file_url} />;
     case 'video':
-      return <video className="video-player" controls width="100%" height="auto" src={item.file_url} />;
+      return <video className="video-player mt-1" controls width="100%" height="auto" src={item.file_url} />;
     default:
       return null;
   }
